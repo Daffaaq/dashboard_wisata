abcc6b9c6503973d5f2059bd6d8f491a1d28865e
Daffaaq <daffaaqila48@gmail.com>
2023-05-16 17:08:05 +0700

    first commit

---
+++ app/Http/Controllers/WisataController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wisata;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class WisataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //pengambilan data barang di tabel barang
+        $data = data_Wisata::all();
+
+        // mengirim data ke view index
+        return view('dashboard.wisata.wisata', ['wisata' => $data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Barang  $barang
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Barang $data)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Barang  $barang
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Barang $data)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Barang  $barang
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Barang $data)
+    {
+
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Barang  $barang
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Barang $barang)
+    {
+        if ($barang->gambar) {
+            Storage::delete($data->gambar);
+        }
+
+        Barang::destroy($data->id);
+        return redirect('dashboard/barang')->with('success', 'Data barang berhasil dihapus');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+//dashboard barang
+// Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->middleware('auth');
+// Route::resource('/dashboard/barang', DashboardBarangController::class)->middleware('auth', 'ceklevel:admin');
+
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('home');
+//dashboard barang
+Route::resource('/dashboard/wisata', WisataController::class)->middleware('auth');
